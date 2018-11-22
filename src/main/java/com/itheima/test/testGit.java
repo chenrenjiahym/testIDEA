07a0ca4debde2b948d648c59fc5c34985ab481da
chenrenjiahym <45219306+chenrenjiahym@users.noreply.github.com>
2018-11-22 14:17:54 +0800

    删除,重试

--- src/main/java/com/itheima/test/testGit.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.test;
-
-public class testGit {
-    public static void main(String[] args) {
-        System.out.println("test");
-
-        System.out.println("teee");
-    }
-}
